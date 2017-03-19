df3964837d7cae345346a6141d499258ba92b081
Tom Wong <tomwong1027@gmail.com>
2017-03-19 23:07:41 +0800

    more html page--restaurant, login, registration

--- EatGlasgowApp/views.py
+++ EatGlasgowApp/views.py
@@ -4,24 +4,30 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 def index(request):
-    
+    RestaurantList={}
     restaurantExample = {"RestaurantID":1, "Name":"Myrestaurant", "Cuisine":"Chinese",
         "StressAdress":"Chinese", "PriceRange":"Expensive", "OpeningHours":"7am-8am",
         "Status":1,"logo":"logo.jpg"}
     response = render(request, 'index.html', restaurantExample)
     # Render the response and send it back!
     return response
-    #return HttpResponse("Rango says hey there partner!")
-    
+	
+def restaurant(request, RestaurantID):
+	
+	restaurantExample = {"RestaurantID":1, "Name":"Myrestaurant", "Cuisine":"Chinese",
+    "StressAdress":"Chinese", "PriceRange":"Expensive", "OpeningHours":"7am-8am",
+    "Status":1,"logo":"logo.jpg"}
+	try:
+		if(int(RestaurantID)==restaurantExample["RestaurantID"]):
+			return render(request,'restaurant.html', restaurantExample)
+		else:
+			return HttpResponse("There is not such restaurant")
+	except ValueError:
+		return HttpResponse("There is not such restaurant")
 
-def index1(request, IndexPage):
-    context_dict = {'Index': IndexPage}
-    # Render the response and send it back!
-    return render(request, 'index.html', context_dict)
-    #return HttpResponse("Rango says hey there partner!")
-    
+
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'login.html', restaurantExample)
     """#f the request is a HTTP POST, try to pull out the relevant information.
     if request.method == 'POST':
         # Gather the username and password provided by the user.
@@ -53,7 +59,6 @@ def login(request):
     else:
         # No context variables to pass to the template system, hence the
         # blank dictionary object..."""
-        
-    
-        
 		
+def registration(request):
+	return render(request, 'registration.html')
