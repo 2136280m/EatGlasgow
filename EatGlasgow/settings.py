f8ebfb5f3314b4e155ade7d9a3dee7f8e16b2ec6
Alex Marshall <Alex Marshall>
2017-03-22 15:35:19 +0000

    tweaked settings and a few things

--- EatGlasgow/settings.py
+++ EatGlasgow/settings.py
@@ -99,6 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -129,3 +130,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_URL = '/static/'
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
