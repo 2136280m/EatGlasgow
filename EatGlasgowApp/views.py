b3acf8a315df698a0366dad5bac980d708234843
Tom <Tom@MacBook-Air.local>
2017-03-23 17:03:56 +0000

    login logout

--- EatGlasgowApp/views.py
+++ EatGlasgowApp/views.py
@@ -81,3 +81,10 @@ def random_restaurant():
         for RN in randomint:
             restaurant_List.append(Restaurant.objects.get(resID=RN))
     return restaurant_List
+
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+    # Take the user back to the homepage.
+    return HttpResponseRedirect(reverse('index'))
