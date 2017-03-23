d606010f26d8bddffa46086b252c0c3c1704984e
carolinelhy <caroline1515@yahoo.com.hk>
2017-03-23 21:37:34 +0000

    More on add restaurant

--- EatGlasgowApp/forms.py
+++ EatGlasgowApp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from EatGlasgowApp.models import UserProfile
+from EatGlasgowApp.models import UserProfile, Restaurant
 
 
 class UserForm(forms.ModelForm):
@@ -13,3 +13,23 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password','isOwner')
+
+class RestaurantForm(forms.ModelForm):
+	CUISINE_CHOICE = (('WE', 'Western'), ('AS', 'Asian'), ('ME', 'Middle Eastern'),)
+	RANGE_CHOICE = ((0, 'Not Rated'), (1, 'Low Price'), (2, 'Affordable'), (3, 'Fancy'),)
+	RESTAURANT_STATUS_CHOICE = ((1, 'Open'), (0, 'Close down'), (-1, 'Close'))
+
+	#name, streedAdress, opening hourt(input)
+	name = forms.CharField(max_length=128, help_text="Please enter the name of restaurant")
+	cuisine = forms.ChoiceField(widget=forms.RadioSelect, choices=CUISINE_CHOICE)
+	streetAdress = forms.CharField(max_length=128, help_text="Please enter the adress of restaurant")
+	priceRange = forms.ChoiceField(widget=forms.RadioSelect, choices=RANGE_CHOICE)
+	openingHours = forms.CharField(max_length=128, help_text="Please enter the opening hours of restaurant")
+	status = forms.ChoiceField(widget=forms.RadioSelect, choices=RESTAURANT_STATUS_CHOICE)
+	description = forms.CharField(max_length=256, help_text="Please enter the description for restaurant")
+
+
+	class Meta():
+		model = Restaurant
+		fields = ('resID', 'owner', 'name', 'photo', 'cuisine', 'streetAdress',
+		'priceRange', 'openingHours', 'status', 'description' )
