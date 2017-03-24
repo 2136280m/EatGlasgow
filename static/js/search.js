a16e7463d0bc06906d641c5ba77a4749c0c1782e
Tom <Tom@MacBook-Air.local>
2017-03-24 16:41:11 +0000

    ajax commit

--- static/js/search.js
+++ static/js/search.js
@@ -1,23 +1,23 @@
 function showHint(str) {
     var xmlhttp;
-    if (str.length == 0) {
-        document.getElementById("txtHint").innerHTML = "";
+    if (str.length == 0) {//if contant of search is null
+        document.getElementById("txtHint").innerHTML = "";//the tab is null(no result)
         return;
     }
     if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp = new XMLHttpRequest();
+        xmlhttp = new XMLHttpRequest();//make Request
     }
     else {// code for IE6, IE5
         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
     xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {//successly received
             document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
-        }
+        }//display the result
         else {
-            document.getElementById("txtHint").innerHTML = "Fail";
+            document.getElementById("txtHint").innerHTML = "Fail";//opps, fail
         }
     }
-    xmlhttp.open("GET", "search/" + str, true);
-    xmlhttp.send();
+    xmlhttp.open("GET", "search/" + str, true);//set the keyword to send
+    xmlhttp.send();//send!
 }
