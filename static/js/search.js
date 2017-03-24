0c8ca80329eeac9cd1b1316faa819ec948f92a83
Tom <Tom@MacBook-Air.local>
2017-03-24 16:02:07 +0000

    js

--- static/js/search.js
+++ static/js/search.js
@@ -1,27 +1,23 @@
-function search(str)
-{
-var xmlhttp;
-if (str.length==0)
-  {
-  document.getElementById("").innerHTML="";
-  return;
-  }
-if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
-  }
-else
-  {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-xmlhttp.onreadystatechange=function()
-  {
-  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {
-    document.getElementById("searchResult").innerHTML=xmlhttp.responseText;
+function showHint(str) {
+    var xmlhttp;
+    if (str.length == 0) {
+        document.getElementById("txtHint").innerHTML = "";
+        return;
     }
-   }
-  }
-xmlhttp.open("GET","search/"+str, true);
-xmlhttp.send();
+    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    }
+    else {// code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
+        }
+        else {
+            document.getElementById("txtHint").innerHTML = "Fail";
+        }
+    }
+    xmlhttp.open("GET", "search/" + str, true);
+    xmlhttp.send();
 }
